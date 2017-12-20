b0aa49a9d15a663e745655493f4a9ec14e48b3fb
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2017-12-20 11:35:29 -0500

    pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,16 @@ $("#start").on("click", function countdown() {
     if (timeLeft > 0) {
         timeLeft--;
         $("#start").html(timeLeft);
-        console.log("test");    
+        console.log("test");    //timeLeft decreasing very rapidly
+        //check score
     }
     
     else if (timeLeft === 0) {
             clearInterval(timerId);
+            //check score
         } 
+    //else if (submit button clicked)
+        //check score
         
 });
 
