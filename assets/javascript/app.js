5200ef917ec383db3be4eab162b9ec025fe28ae6
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2017-12-20 14:06:26 -0500

    timer functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,31 @@
-//timer created
+//timer created on click
 var timeLeft = 30;
+var timerId;
 
+$("#start").on("click", function(){
+	timerId = setInterval(countdown, 1000);
+});
 
-$("#start").on("click", function countdown() {
-
-    var timerId = setInterval(countdown, 1000);
+function countdown() {
 
     if (timeLeft > 0) {
         timeLeft--;
         $("#start").html(timeLeft);
-        console.log("test");    //timeLeft decreasing very rapidly
         //check score
     }
     
     else if (timeLeft === 0) {
             clearInterval(timerId);
             //check score
-        } 
+    } 
     //else if (submit button clicked)
-        //check score
-        
-});
+        //check score     
+};
 
+//function checkScore () {
+    //if answer selected === correct answer
+        //correct answer++;
+    //else if answer selected !== correct answer
+        //incorrect answer ++;
+    //else if no answer selected
+        //unanswered questions ++;
